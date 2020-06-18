76b62f19195ce046c5a37fcf4800f4defde4f70e
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-06-17 23:19:26 -0700

    added tempory logo

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,12 +1,17 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
-import styles from './NavBar.module.scss'
+import styles from './NavBar.module.scss';
+
+import testLogo from '../../image/testLogo.png';
 
 import {Link} from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div className={styles.container}>  
-                <Link to='/'>Logo, possible on click to home</Link>
+                <Link to='/'>
+                    <img src={testLogo} alt='test image'/>
+                </Link>
                 <div className={styles.category}>
                 <Link to='/beginner'>Beginner</Link>
                 <Link to='/intermediate'>intermediate</Link>
