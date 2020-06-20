76911b339a385170274c78c74c6bc26cbfb90885
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-06-20 10:25:10 -0700

    created basic layout of home page

---
+++ src/component/Home/CategoryCards/CategoryCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryCards = () => {
+    return (
+        <div>
+            <h1>create an array with objects with the value of image and category from there we can map through it to display each. so probably gonna need a cards and a card category should i even separte and just make stick to one with a data component instead.</h1>
+        </div>
+    )
+}
+
+export default CategoryCards;
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import categoryCards from './CategoryCards/CategoryCards'
 
-function LandingPage() {
+import styles from './Home.module.scss'
+import CategoryCards from './CategoryCards/CategoryCards';
+
+const LandingPage = () => {
     return (
-        <div>
-            <h1>LandingPage</h1>
+        <div className={styles.container}>
+            <div className={styles.slider}>
+                <div className={styles.intro}>
+                    <h1>THINK OF A WELCOME SENTENCE</h1>
+                    <h2>add some description of the website and what we offer</h2>
+                </div>
+                <div className={styles.image}>
+                    <h1>have a rotating set of images. probably 1 for each category. Create an array of images and rotate through them probably? this is where the actual "slider" feature will be</h1>
+                </div>
+            </div>
+            <div className={styles.categoryCards}>
+                <CategoryCards/>
+            </div>
         </div>
     )
 }
