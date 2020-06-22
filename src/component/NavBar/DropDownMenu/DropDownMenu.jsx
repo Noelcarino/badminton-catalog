23c92cf2859aea1d4d0df09540fbe21a60d0864a
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-06-21 19:28:30 -0700

    fixed issue with drop down menu, but might need to look into a better approach

--- src/component/NavBar/DropDownMenu/DropDownMenu.jsx
+++ src/component/NavBar/DropDownMenu/DropDownMenu.jsx
@@ -6,10 +6,11 @@ import {Link} from "react-router-dom";
 const DropDownMenu = () => {
     return (
         <div className={styles.container}>
-            <Link to='/apparel'>Apparel</Link>
-            <Link to='/racquet'>Racquet</Link>
-            <Link to='/shoes'>Shoes</Link>
-            <Link to='/strings'>Strings</Link>
+            <Link className={styles.link} to='/apparel'>Apparel</Link>
+            <Link className={styles.link} to='/racquet'>Racquet</Link>
+            <Link className={styles.link} to='/shoes'>Shoes</Link>
+            <Link className={styles.link} to='/strings'>Strings</Link>
+            
         </div>
     )
 }
