69c493e56dec50a98f7a9cff2ba2f68fb52fde73
Noel Carino <leononirac1992@gmail.com>
2020-07-05 10:38:15 -0700

    7/5 | got shoe images to shoe up in apparel

--- src/component/Equipment/Apparel/apparelitem.jsx
+++ src/component/Equipment/Apparel/apparelitem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Apparel.module.scss'
+import shoe from '../../../image/shoes/badminton-shoe.jpeg';
 
 export default class ApparelItem extends React.Component {
     constructor(props){
@@ -11,7 +12,12 @@ export default class ApparelItem extends React.Component {
     render(){
         return (
             <div className={styles.apparel_item}>
-                <div className={styles.apparel_fake_image}>Fake Image</div>
+                <div className={styles.apparel_fake_image}>
+                    <img 
+                    src={shoe}
+                    className={styles.apparel_image}
+                    alt="badminton-shoe"/>
+                </div>
                 <p>Name: {this.props.apparel.apparel_name}</p>
                 <p>Brand: {this.props.apparel.apparel_brand}</p>
                 <p>Size: {this.props.apparel.apparel_size}</p>
